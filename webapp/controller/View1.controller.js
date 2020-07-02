38a82fde6731ea3541f58dc746a7a412cffb4046
Ribeiro Lauro <lauropribeiro@gmail.com>
2020-07-02 01:44:17 +0000

    1. Restricting 'search' functionality only when 8 or more characters are entered. 2. Validating 'CEP' field to just numbers.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox"
+], function (Controller, JSONModel, MessageBox) {
 	"use strict";
 
 	return Controller.extend("lauro.viaCEP.controller.View1", {
@@ -12,31 +13,58 @@ sap.ui.define([
 
 		},
 
-		onSearch: function (oEvent) {
-
-			var oView = this.getView();
-			var oForm = oView.byId("formCEP");
-			var oModel = oView.getModel();
-			var sQuery = oEvent.getParameter("query");
-
-			if (!sQuery) {
-
-				oModel.setData({
-					emptyData: {}
-				});
-
-				oForm.setVisible(false);
-
-				return;
+		onChange: function (oEvent) {
+			var oSearchField = this.getView().byId("sfCEP");
+			var sQuery = oEvent.getParameter("newValue");
+			if (sQuery.length >= 8) {
+				oSearchField.setShowSearchButton(true);
+			} else {
+				oSearchField.setShowSearchButton(false);
 			}
 
-			var sURL = "https://viacep.com.br/ws/" + sQuery + "/json/";
+		},
 
-			$.get(sURL, function (data, status) {
-				oModel.setData(data);
-				oForm.setVisible(true);
-			});
+		onSearch: function (oEvent) {
 
+			function isQueryValid(sQuery) {
+
+				var numbers = /^[0-9]+$/;
+				if (sQuery.match(numbers)) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			var sQuery = oEvent.getParameter("query");
+			if (isQueryValid(sQuery)) {
+
+				var oView = this.getView();
+				var oForm = oView.byId("formCEP");
+				var oModel = oView.getModel();
+
+				if (!sQuery) {
+
+					oModel.setData({
+						emptyData: {}
+					});
+
+					oForm.setVisible(false);
+
+					return;
+				}
+
+				var sURL = "https://viacep.com.br/ws/" + sQuery + "/json/";
+
+				$.get(sURL, function (data, status) {
+					oModel.setData(data);
+					oForm.setVisible(true);
+				});
+
+			} else {
+				MessageBox.error("CEP '" + sQuery + "' inválido");
+			}
 		}
+
 	});
 });
